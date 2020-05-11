6851312d6db26cd681545f7f319f95879efae687
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-11 19:20:22 +0300

    commit

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -2,7 +2,7 @@ let m = 10;
 let n = 50;
 let result = 0;
 for (let m = 10; m <= n; m++) {
-    result = result + m
+   
     if (m % 2 ===0 && m % 4 ===0){
         result + m
     }
@@ -13,9 +13,11 @@ for (let m = 10; m <= n; m++) {
         result * m
         
     }
-    if(m % 5){
+    if(m % 5 ===){
         console.log(m)
     }
+    result = result + m
+    
 }
 
 
