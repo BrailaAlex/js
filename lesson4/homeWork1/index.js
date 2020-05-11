f066e8982a9dbde54401e88feb89f16f707b1aa9
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-11 18:13:38 +0300

    commit

--- lesson4/homeWork1/index.js
+++ lesson4/homeWork1/index.js
@@ -1,18 +1,18 @@
 const n = 7;
 let sum = 0;
 
-for (i = 1; i <= n; i++) {
+for (let i = 1; i <= n; i++) {
     
-    sum = sum +i;
-   
-    console.log(i = String(i))
-   
-    
-    
-
-
-
-
-
+    sum = sum + i +'';
 }
+    console.log(sum)
+   
+    
+    
+
+
+
+
+
+
 
