896ae07f8e54e4de3d122211b164f616926a130b
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-12 19:24:14 +0300

    commit

---
+++ lesson5/task3/index.js
@@ -0,0 +1,12 @@
+function getEvenOdd(number) {
+    if (number % 2 ===0) {
+        return console.log('even');
+        
+        
+    }else{
+        return console.log('odd');
+        
+    }
+    
+}
+
