dc249ef871b838cb409cbe1594649d1fa7091fa1
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-12 18:00:08 +0300

    commit

--- lesson4/task6/index.js
+++ lesson4/task6/index.js
@@ -2,14 +2,14 @@ let sum = 0;
 
 for (let i = 0; i <= 1000; i++) {
 
-    if (i % 2 === 1) {
+    if (i % 2 === 0) {
         console.log("Found")
         sum += i;
-    } else if (sum * 5 > 5000) {
+    } else if (sum * 5 < 5000) {
         console.log("Bigger")
 
     } else {
-        console.log("Smaller or equal")
+        console.log("Smaller")
     }
 
 
