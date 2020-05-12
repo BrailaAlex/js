af140fc3b6c681dd35aebf209932d868b49b47b5
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-12 20:38:00 +0300

    commit

---
+++ lesson5/HW1/index.js
@@ -0,0 +1,26 @@
+
+function getSum(num1, num2) {
+    let sumEven = 0;
+     for (let i = num1; i <= num2; i++) {
+        if (i % 2 == 0) {
+            sumEven += i
+         
+        }
+          
+    }
+
+    return sumEven
+
+}
+
+//console.log(getSum(4, 10));
+
+
+
+
+
+
+
+
+
+
