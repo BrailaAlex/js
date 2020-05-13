c5bc039cc635d8ddfa0c60d313223323abf9a4ed
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-13 16:38:23 +0300

    commit

--- lesson5/HW2/index.js
+++ lesson5/HW2/index.js
@@ -1,14 +1,22 @@
 function getPrimes(num) {
   for (let i = 2; i <= num; i++) {
-    if (i % 1 === 0) {
-      console.log(i);
-
+    let numPr = true;
+    for (let j = 2; j * 2 <=i; j++){
+      if (i % j === 0){
+        numPr = false;
+      }
     }
+    if (numPr && i > 1){
+        console.log(i);
+    }
+    
   }
 
 }
 
-//getPrimes(10);
+//getPrimes(10)
+
+
 
 
 
