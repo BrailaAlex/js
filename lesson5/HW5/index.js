a9c263bbefaa099ef72f04ba7bbf6de9c0d160a1
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-13 17:21:39 +0300

    commit

--- lesson5/HW5/index.js
+++ lesson5/HW5/index.js
@@ -1,14 +1,11 @@
+
 function findDivCount(a, b, n) {
     let amount = 0;
     for (let i = a; i <= b; i++)
         if (i % n == 0) {
         amount += i ;
-            console.log(i);
-            //console.log(amount);
-
-
+            console.log(amount);
         }
-
+return amount;
 }
-
-//findDivCount(2, 6, 2);
+//findDivCount(2,4,2)
