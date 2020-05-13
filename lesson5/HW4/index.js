17c6b5cc3a82eaa8181beb1c122aec57a1e5c8c5
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-13 14:06:18 +0300

    commit

---
+++ lesson5/HW4/index.js
@@ -0,0 +1,23 @@
+function sum (from,to){
+    let sumab =0;
+    for(let i =from; i<=to;i++){
+         sumab+=i;
+
+    }
+    return sumab
+}
+//console.log(sum(1,5,));
+
+function compareSum(a,b,c,d){
+    if(sum(a,b) > sum(c,d)){
+        return console.log(true);
+        
+    }else
+    {return console.log(false);
+    
+
+
+     }
+
+}
+compareSum(1,2,3,4)
