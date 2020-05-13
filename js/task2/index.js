6fd47f4e6dadac4efbe3bef98206388bd04d93d3
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-13 11:18:23 +0300

    commit

--- js/task2/index.js
+++ js/task2/index.js
@@ -1,29 +1,26 @@
 let m = 10;
 let n = 40;
 let result = 0;
-
 for (let i = m; i <= n; i++) {
 
     if (i % 2 === 0 && i % 4 !== 0) {
-        // console.log('1 if is called' + i);
         result += i
     }else if (i % 3 === 0) {
-        // console.log('1 if is called' + i)
         result -= i
     }
      else if (i % 4 === 0) {
-           // console.log('2 if is called' + i);
         result *= i
 
     }
     if (i % 5 === 0) {
-        // console.log('3 if is called' + i)
         console.log(i)
     }
 
 
 
 }
+
+
 
 
 
