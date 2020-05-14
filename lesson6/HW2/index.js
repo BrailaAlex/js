d11bbbfd799d1a45aba53a240f102bcf7367305e
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-14 23:31:51 +0300

    commit

---
+++ lesson6/HW2/index.js
@@ -0,0 +1,14 @@
+//let numbers = [1,2,3,4,5];
+
+function squareArray(arr){
+    let square = new Array;
+    for (i in arr){
+        square[i] = Math.pow(arr[i], 2)
+    }
+   return square;
+        
+    }
+
+
+//let arr = squareArray(numbers)
+//console.log(arr);
