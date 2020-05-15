85e386bc9b57eeb1229beb51e645a99229abf29f
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-15 11:31:46 +0300

    commit

--- standups/task4/index.js
+++ standups/task4/index.js
@@ -1,35 +1,36 @@
-//1. learn requirements & create algo
-//2. create prototype solution
-//3. refactoring & testing
-//4. final solution
+//1.learn reqeirements & create algo
+//2.create prototype solution
+//3.refactoring & testing 
+//4.final solution
+
+//1 - true,2 - true,3 - false,4 - false, 5 - false,6 - true.
 
 
-//algo
-//1. iterate from 1 to n
-//2. write func that checks number prime
-//3. if number is prime -print it
-
-//input; number
-//output:boolean
-
-//algo
-//1. iterate from 1 to num
-//2. if num % i !==0 - false
+//algo.
+//1.iterate from 1 to num +++
+//2.if (num % i === 0 - false)+++
 
 
+// input:num;
+// output:boolean;
 function isPrime(num) {
     for (let i = 2; i < num; i++) {
-        //console.log('Check for: ' + i);
-        if (num % 1 === 0) {
-            //console.log('If is called for: ' + i);
+        //console.log("if is NOT  called for i: " + i);
+        if (num % i === 0) {
+            //console.log("if is NOT  called for i: " + i);
             return false;
         }
+        //console.log("if is NOT  called for i: " + i);
     }
-    //console.log('If is NOT called for: ' + i);
+    //console.log("loop is finished");
     return true;
 }
-//input; number
-//output:undefined
+//algorithm.
+//1.iterate from i to n +++
+//2.write func that chech number prime.
+//3.if number is prime - print it +++
+// input:n;
+// output:undefined;
 function getPrimes(n) {
     for (let i = 2; i <= n; i++) {
         if (isPrime(i)) {
