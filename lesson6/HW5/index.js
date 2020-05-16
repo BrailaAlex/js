b9bb2c99eb14bd83f29a4f8b7a2b5937cfd317b5
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-16 18:42:08 +0300

    commit

---
+++ lesson6/HW5/index.js
@@ -0,0 +1,35 @@
+//let numbers = [1, 2, 3, 4, 5];
+
+function reverseArray(arr){
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+   
+
+
+   return arr.reverse(); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let arr = reverseArrey(numbers);
+//console.log(arr);
