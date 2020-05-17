c50fc51bbc5dcf0a216d0f7fa26c93f9c732100b
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-17 20:25:27 +0300

    commit

--- lesson6/HW9/index.js
+++ lesson6/HW9/index.js
@@ -1,31 +1,23 @@
-let clients = ['Ann', 'john', 'User'];
+let clients = ['Ann', 'John', 'User'];
 let balances = [1400, 87, -6];
-let client = 'John';
-let amount = 50;
-
-
-let indexArr;
 
 function withdraw(clients, balances, client, amount) {
 
+    let indexOfArray;
+    
     for (let i = 0; i < clients.length; i++) {
-        if (clients[i] == client) {
-            indexArr = i;
+        if (clients[i] === client) {
+            indexOfArray = i;
         }
-
     }
-    if (balances[indexArr >= amount]) {
-        return (balances[indexArr] = balances[indexArr] - amount)
+    if (balances[indexOfArray] >= amount) {
+        return (balances[indexOfArray] = balances[indexOfArray] - amount);
     } else {
         return -1;
     }
-
 }
 
 
-
-
-
-//let balanceMoney = withdraw(clients, balances, clients, amount);
+//let balanceMoney = withdraw(clients, balances, clients);
 
 //console.log(balanceMoney);
