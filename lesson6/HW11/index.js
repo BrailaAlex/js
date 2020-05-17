995fd9e5171252ec6536f3dd6cd471ff85c493e7
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-17 17:33:39 +0300

    commit

---
+++ lesson6/HW11/index.js
@@ -0,0 +1,31 @@
+//let numbers = [1, 2, 3, 4, 5];
+
+function includes(arr, number) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === number) {
+            return true
+        }
+
+    }
+
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let arr1 = includes(numbers, 20);
+//console.log(arr1);
