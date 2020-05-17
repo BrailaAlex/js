a8b932d87139fabadcb865809ed3ff58653abfc3
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-17 17:23:27 +0300

    commit

---
+++ lesson6/HW10/index.js
@@ -0,0 +1,19 @@
+//let numbers = [1, 2, 3, 4, 5];
+
+function getSubArray(arr, number){
+
+    return arr.slice(0, number)
+
+
+}
+
+
+
+
+
+
+
+
+
+//let arr1 = getSubArray(numbers, 2);
+//console.log(arr1);
