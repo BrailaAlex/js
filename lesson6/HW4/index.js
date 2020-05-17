1148b094b3cd0c6db6aa84ac0793cc91fdb878e6
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-17 16:30:50 +0300

    commit

--- lesson6/HW4/index.js
+++ lesson6/HW4/index.js
@@ -1,41 +1,43 @@
-const numbers = [1, 2, 3, 4, 5];
 
-function increaseEvenEl(arr, delta) {
-    if (!Array.isArray(arr)) {
+//const numbers = [1, 2, 3, 4, 5];
+
+
+function increaseEvenEl(numbers, delta) {
+
+    if (!Array.isArray(numbers)) {
+
         return null;
-    }
 
-    let evenNumbers = [];
-   
-    for (let i = 0; i < arr.length - 1; i++) {
-
-         if(numbers % 2 === 0){
-          evenNumbers.push(numbers[i] + delta);
-                   
+    } else {
+        let evenNumbers = [];
+        for (let i = 0; i <= numbers.length - 1; i++) {
+            if (numbers[i] % 2 === 0) {
+                evenNumbers.push(numbers[i] + delta);
+            }
+            if (numbers[i] % 2 === 1) {
+                evenNumbers.push(numbers[i]);
+            }
         }
-        if(numbers[i] % 2 === 1){
-            evenNumbers.push(numbers[i]);
-            
-        }
-       
-        
-        
-                
-        
+        return evenNumbers;
     }
-
-    return evenNumbers;
-   
 }
 
 
+        //let arr = increaseEvenEl(numbers, 10);
+        //console.log(arr);
 
 
 
 
 
-let arr = increaseEvenEl(numbers, 10);
-console.log(arr);
+
+
+
+
+
+
+
+
 
 
 
