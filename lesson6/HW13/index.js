7ec887d60ac34d8e61b8c0a1443e72d9bd1fe988
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-17 20:20:37 +0300

    commit

--- lesson6/HW13/index.js
+++ lesson6/HW13/index.js
@@ -2,23 +2,19 @@
 
 function uniqueCount(arr) {
     if (!Array.isArray(arr)) {
-
         return null;
     }
+    for (let i = 0; i <= arr.length; i++) {
 
-    for (let i = 0; i < arr.length; i++) {
-         for(let j = i + 1; j <= arr.length; j++){
-            if(arr[i] == arr[j]){
-                arr.splice(j, 1)
+        for (let j = i + 1; j <= arr.length; j++) {
+            
+            if (arr[i] === arr[j]) {
+                arr.splice(j, 1);
+                j--;
+            }
         }
-
-
-        }
-        return arr.length;
-
-    
-   }
-
+    }
+    return arr.length;
 }
 
 
