4a57c57f477cbc2e4081f2aabd540933e629eebe
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-18 10:21:28 +0300

    commit

--- standups/18.05.2020/task5/index.js
+++ standups/18.05.2020/task5/index.js
@@ -1,12 +1,53 @@
 
-let n = 20;
-let sum = '';
-let i = 1;
+let numbers = [1, 2, 3, 4, 5];
+//input: array of numbers
+//output: array of numbers
 
+function squareArray(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
 
-while (i <= n) {
-    //console.log("Loop is Called");
-    sum += i;
-    i++;
+    }
+    //1 good
+    let res = [];
+    //let newArr = [...arr];
+    for (let el of arr) {
+        //1
+
+        // res[i] = arr[i] * arr[i];
+
+        //2
+        res.push(el * el);
+
+        //not good
+        //Mach.pow(arr[i], 2)
+    }
+    return res;
+
 }
-console.log(sum);
+
+
+function squareArray2(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+
+    }
+
+    let res = [...arr];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        res[i] * arr[i] * arr[i];
+    }
+    return res;
+
+}
+
+
+
+
+
+
+
+let arr = squareArray(numbers)
+console.log(arr);
