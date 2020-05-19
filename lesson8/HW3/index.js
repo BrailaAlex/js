004e5e5c44fc6ecfaf84768d46a5e43b3512a1b5
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-19 18:51:45 +0300

    commit

---
+++ lesson8/HW3/index.js
@@ -0,0 +1,18 @@
+
+const user = {
+    name: 'Bob',
+    age: 25,
+}
+
+
+function getKeys(obj){
+    let arr = Object.keys(obj);
+    return  (arr.forEach(pomp => console.log(pomp)));
+
+   
+
+}
+
+
+ getKeys(user)
+
