8a3f8cd8d1f546f1b102ce58b09092b346fa6fff
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-19 06:18:59 +0300

    commit

---
+++ lesson7/HW8/index.js
@@ -0,0 +1,19 @@
+//const numbers = [1, 2, 3, 4, 5];
+
+const arrAverange = arr => {
+    if (!Array.isArray(arr)) {
+        return null;
+
+    }
+    return arr.reduce((a, b) => (a + b) / arr.length);
+
+
+
+
+}
+
+
+
+
+//let arr = arrAverange(numbers);
+//console.log(arr);
