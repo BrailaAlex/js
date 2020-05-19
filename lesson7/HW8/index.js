2d31136438717e91a596483460f9c7576857528c
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-19 06:54:34 +0300

    commit

--- lesson7/HW8/index.js
+++ lesson7/HW8/index.js
@@ -1,12 +1,12 @@
 const numbers = [1, 2, 3, 4, 5];
 
-function arrAverange(nums) {
+function arrAverange(arr) {
     if (!Array.isArray(arr)) {
         return null;
 
     }
     
-    return arr.reduce((a, b) => (a + b)) / nums.length;
+    return arr.reduce((a, b) => (a + b)) / arr.length;
 
 
 
