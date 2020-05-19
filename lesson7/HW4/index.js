bfb88601084c16184437b35f862827f255b88578
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-19 05:14:57 +0300

    commit

---
+++ lesson7/HW4/index.js
@@ -0,0 +1,25 @@
+//const numbers = [1, 2, 3, 4, 5];
+
+//const cloneArr = arr =>{
+    if (!Array.isArray(arr)) {
+        return null;
+
+}else {
+    return numbers.slice();
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+//let arr = cloneArr(numbers, 10);
+//console.log(arr);
