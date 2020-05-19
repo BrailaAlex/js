d9f6f1fe718b7b46fa1a1d4938529f6bfe0dff0e
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-19 21:27:39 +0300

    commit

---
+++ lesson9/task2/index.js
@@ -0,0 +1,18 @@
+/*const obj = {
+'John Doe': 19,
+'Tom': 17,
+'Bob': 18,
+'Sara': 20,
+'Nicolas': 17,
+'Artur':20,
+};*/
+ const copyObj = obj => Object.assign({}, obj)
+
+ 
+
+
+
+
+
+ //let object = copyObj(obj);
+ //console.log(object);
