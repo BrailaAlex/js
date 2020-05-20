0b08df9d38a4eefa9548e73bde71ce9749db86d6
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-20 11:54:51 +0300

    commit

---
+++ lesson10/task1/index.js
@@ -0,0 +1,24 @@
+arrNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function getFiniteNumbers(arr) {
+
+}
+
+function getFiniteNumbersV2(arr) {
+
+}
+
+function getNaN(arr) {
+
+}
+
+function getNaNV2(arr) {
+
+}
+
+function getIntegers(arr) {
+
+}
+
+let arr1 = getFiniteNumbers(arrNumbers)
+console.log(arr1);
