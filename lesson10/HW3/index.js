3530edf1cc6a7b5d1ea3e955fbb976d652e75b43
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-20 15:31:11 +0300

    commit

--- lesson10/HW3/index.js
+++ lesson10/HW3/index.js
@@ -1,6 +1,6 @@
 
-//let number = 17.5585282;
-//let n = 2;
+let number = 17.5585282;
+let n = 2;
 
 
 const superRound = (number, n) =>
@@ -8,11 +8,11 @@ const superRound = (number, n) =>
         Math.floor(number * 10 ** n) / 10 ** n,
         Math.round(number * 10 ** n) / 10 ** n,
         Math.ceil(number * 10 ** n) / 10 ** n,
-        Math.trunc(number * 10 ** n) / 10 ** n,
-        + number.toFixed(n)
- 
+        Math.trunc(number * 10 ** n) / 10 ** n, + number.toFixed(n)
+
+
     ]
 
 
 
-//console.log(superRound(number, n));
+console.log(superRound(number, n));
