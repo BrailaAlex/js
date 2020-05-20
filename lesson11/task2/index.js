014a37b5fe6e1f81b4b2bf7a031226c90d0c7b04
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-20 17:18:26 +0300

    commit

---
+++ lesson11/task2/index.js
@@ -0,0 +1,40 @@
+const contacts = [
+    {
+        name: 'Tom',
+        phoneNumber: '222-22-22'
+    },
+    {
+        name: 'Ann',
+        phoneNumber: '333-33-33'
+    },
+    {
+        name: 'Sam',
+        phoneNumber: '444-44-44'
+    },
+    {
+        name: 'Kate',
+        phoneNumber: '555-55-55'
+    },
+    {
+        name: 'Bill',
+        phoneNumber: '666-66-66'
+    },
+];
+
+const sortContacts = contacts =>{
+    if(!Array.isArray(contacts)){
+        return null;
+    };
+    const result = contacts
+    .sort((a, b) => {
+        return a.name.localeCompare(b.name);
+    });
+
+    return result;
+
+};
+
+
+
+
+console.log(sortContacts(contacts));
