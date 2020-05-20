f97b49bfdc5747b643c6601ed92db6dcab57279a
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-20 16:50:15 +0300

    commit

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -1,33 +1,18 @@
-
-
 const splitText = (text, len) => {
-    if (typeof text !== 'string') {
-        return null
-    };
     const strArr = [];
     let startPosition = 0;
 
 
-    while (true) {
-         
-        let chunk = text.substr(0, len);
-        strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
-        startPosition +=0;
-
-        if(chunk.length ===0){
+    while(true){
+        let chunk = text.substr(startPosition, len);
+        if(chunk.length === 0){
             break;
         }
+        strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
+        startPosition += len;
+        
     }
+    return strArr.join('\n');
+}
 
-
-return strArr.join('\n');
-
-
-};
-
-
-
-
-
-
-console.log(splitText('abcdfhgiklm', 4));
+console.log(splitText('abcdefjijklm', 4));
