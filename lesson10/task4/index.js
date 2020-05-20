b6bf3a2720952953ed2aa4658250e4c66a646154
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-20 13:21:57 +0300

    commit

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,17 +1,17 @@
 //input: arr
 //input: max num in arr
 
-const arr = [2, 8, 52, -25, 6, -62, 5.25, 6.24, -5, 28];
+const arr = [52, -35, 88, -3, 25, 87, 14, -6, -8, -45];
 
-const getMaxAbsoluteNumber = arr =>{
-    if (!Array.isArray(arr)) {
+const getMaxAbsoluteNumber = arr => {
+    if (!Array.isArray(arr) || (arr.length == 0)) {
         return null;
     }
     return Math.max.apply(null, arr);
-    
+
 
 
 }
 
-//let result = getMaxAbsoluteNumber(arr);
-//console.log(result);
+let result = getMaxAbsoluteNumber(arr);
+console.log(result);
