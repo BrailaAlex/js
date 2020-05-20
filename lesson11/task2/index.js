8e6048cf8bb5cbe2ab17d53c72a3a250f62dd263
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-20 17:19:14 +0300

    commit

--- lesson11/task2/index.js
+++ lesson11/task2/index.js
@@ -27,7 +27,7 @@ const sortContacts = contacts =>{
     };
     const result = contacts
     .sort((a, b) => {
-        return a.name.localeCompare(b.name);
+        return b.name.localeCompare(a.name);
     });
 
     return result;
