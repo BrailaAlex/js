ce782bd409f5520d49fae3ff84f88ff56b8934f4
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-21 13:39:09 +0300

    commit

--- lesson13/task2/index.test.js
+++ lesson13/task2/index.test.js
@@ -2,15 +2,18 @@ import getSum, { getSquaredArray, getOddNumbers } from './calculator';
 
 it('should get squared numbers', () => {
     const result = getSquaredArray([1, 2, 3]);
+    
 
     expect(result).toEqual([1, 4, 9])
 });
+
 
 it('should get odd numbers', () => {
     const result = getOddNumbers([1, 2, 3]);
 
     expect(result).toEqual([1, 3])
 });
+
 
 it('should get sum of numbers', () => {
     const result = getSum(8, 4);
