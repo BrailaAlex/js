cb048afde0a3844b8bfc4c62a41cb82d2de708ac
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-21 20:13:28 +0300

    commit

--- lesson14/task2/index.js
+++ lesson14/task2/index.js
@@ -1,31 +1,31 @@
-
-
-
 const createMessenger = () => {
 
-    let message = 'Just learn it';
-    const sender = 'Gromcode';
+    let message = 'Just learn it!';
+    let sender = 'Gromcode';
 
     function sendMessage(name) {
-        console.log(`${name}, ${message}! Your ${sender}`);
+        console.log(`${name}, ${message} Your ${sender}`);
 
-    }
+    };
 
     function setMessage(text) {
         message = text;
-    }
+
+
+    };
 
     function setSender(text) {
         sender = text;
+
     }
     return {
         sendMessage,
         setMessage,
         setSender,
     };
-
 };
 export default createMessenger;
+
 
 const messenger1 = createMessenger();
 
@@ -33,10 +33,15 @@ const messenger2 = createMessenger();
 
 const messenger3 = createMessenger();
 
-
 messenger1.sendMessage('Bob');
-messenger2.setMessage('Good job')
-messenger2.sendMessage('Bob')
+
+
+messenger2.setMessage('Good job!');
+
+messenger2.sendMessage('Bob');
+
 
 messenger3.setSender('Gromcode');
+
+
 messenger3.sendMessage('Bob');
