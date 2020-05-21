c9392431326afc3214ee6112c9139041cee6e3e8
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-21 11:20:59 +0300

    commit

--- standups/21.05.2020/filter/index.js
+++ standups/21.05.2020/filter/index.js
@@ -1,3 +1,6 @@
+//FILTER
+
+
 //input: array, function
 //output: array
 
@@ -26,9 +29,53 @@ function filterArrayElements(arr, callback) {
 }
 
 
-const numbers = [1, 2, 3, 4, 5, 100, 40, 44, 55, 1234];
+/*const numbers = [1, 2, 3, 4, 5, 100, 40, 44, 55, 1234];
 let callback = (el) => el % 2 === 0;
 
 let res = filterArrayElements(numbers, callback);
+console.log(res);*/
+
+
+//MAP
+
+//input: array, function
+//output: array
+
+
+//algo:
+//1 create new array
+//2 iterate array
+//3 call callback for every elem and add result to array
+
+
+
+function mapArrayElements(arr, callback) {
+    let mapped = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let res = callback(arr[i], i, arr);
+
+        mapped.push(res);
+    }
+
+    return mapped;
+
+}
+
+
+const numbers = [1, 2, 3, 4, 5, 100, 40, 44, 55, 1234];
+let callback = (el) => el += 100;
+
+let res = mapArrayElements(numbers, callback);
 console.log(res);
+
+
+//SORT
+
+const numbers1 = [1, 200, 39, 44, 55, 100, 40, 44, 55, 1234];
+numbers1.sort((el1, el2) => el1 - el2);
+console.log(numbers1.sort((el1, el2) => el1 - el2));
+
+
+
 
