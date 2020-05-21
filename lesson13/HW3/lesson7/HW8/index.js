6345c46fca008c98a1ca2a827dc5574c4268e60b
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-21 16:46:50 +0300

    commit

---
+++ lesson13/HW3/lesson7/HW8/index.js
@@ -0,0 +1,20 @@
+const numbers = [1, 2, 3, 4, 5];
+
+function arrAverage(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+
+    }
+    
+    return arr.reduce((a, b) => (a + b)) / arr.length;
+
+
+
+
+}
+
+
+
+
+let arr1 = arrAverage(numbers);
+console.log(arr1);
