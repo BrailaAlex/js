141464750333e3e9f847684b6d54556647b9a3f3
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-25 19:18:05 +0300

    commit

--- lesson16/HW1/index.js
+++ lesson16/HW1/index.js
@@ -1,14 +1,15 @@
 //input : number;
 
-const array = [];
+
 
 export function createArrayOfFunctions(number) {
-    if (typeof (number) === number) {
+    const array = [];
+    if (typeof (number) !== "number") {
         return null;
     } else if (number === undefined) {
         return array;
     } else {
-        for (let i = 0; i < 0; i++) {
+        for (let i = 0; i < number; i++) {
             array[i] = function () {
                 return i;
             }
