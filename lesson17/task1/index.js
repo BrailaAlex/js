82e2db9d17a2cbedc630ec701056cb697b7ea59e
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-26 20:16:18 +0300

    commit

---
+++ lesson17/task1/index.js
@@ -0,0 +1,15 @@
+
+export const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+};
+
+
+user.getFullName = function(){
+    return console.log(user.firstName + " " +  user.lastName);
+     ;
+}
+
+
+
+//user.getFullName();
