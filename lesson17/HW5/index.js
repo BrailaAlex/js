9bdaeb589889763f3fb2976d1204935239c3e614
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-28 14:30:24 +0300

    commit

---
+++ lesson17/HW5/index.js
@@ -0,0 +1,39 @@
+
+
+const timer = {
+    secondsPassed: 0,
+    minsPassed: 0,
+    id: 0,
+
+    startTimer() {
+        this.id = setInterval(() => {
+            this.secondsPassed++;
+            if (this.secondsPassed == 60) {
+                this.secondsPassed = 0;
+                this.minsPassed++;
+            }
+
+        }, 1000);
+    },
+
+
+
+
+
+    getTime() {
+        return `${this.minsPassed}: ${this.secondsPassed}`;
+
+
+    },
+    stopTimer() {
+        clearInterval(this.id)
+
+    },
+    resetTimer() {
+        this.secondsPassed = 0;
+        this.minsPassed = 0;
+
+    }
+}
+console.log(timer.getTime());
+console.log(timer.stopTimer());
