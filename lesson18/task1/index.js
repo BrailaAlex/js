b483ec0e231c591adeee7aaa8576966720b54f61
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-05-28 16:24:25 +0300

    commit

---
+++ lesson18/task1/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+export const event = {
+    message: 'Well come to the party!',
+    guests: [
+        { name: 'John', age: 18, email: 'joni_li@server.com' },
+        { name: 'Ann', age: 19, email: 'ann_grid@server.com' },
+        { name: 'Bob', age: 19, email: 'bobik@server.com' },
+        { name: 'Aline', age: 16, email: 'aline_gray@server.com' },
+        { name: 'Miki', age: 17, email: 'miki_rurk@server.com' }
+    ],
+
+
+    getInvitations() {
+        return this.guests
+            .filter(({ age }) => age >= 18)
+            .map(({ name, email }) => ({
+                email, message: `Hi ${name}, ${this.message}`
+
+            }));
+        
+       
+
+    }
+};
+
+
+
+console.log(event.getInvitations());
+
