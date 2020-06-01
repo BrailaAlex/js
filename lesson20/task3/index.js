0543d264567cfc5e25b88de3277c9d7ecb10dc55
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-01 18:22:27 +0300

    commit

--- lesson20/task3/index.js
+++ lesson20/task3/index.js
@@ -1,2 +1,35 @@
 
- 
+
+ export class Wallet {
+    
+        _balance = 0
+    
+    getBalanse(){
+        return this._balance;
+    }
+
+    deposit(amount) {
+        this._balance += amount;
+    }
+
+    withdraw(amount){
+        if(amount > this._balance){
+            console.log('No enouhg funds');
+
+            return;
+            
+        }
+        this._balance -= amount;
+    }
+
+}
+//const wallet1 = new Wallet();
+
+//console.log(wallet1.getBalanse());
+//wallet1.deposit(500);
+//console.log(wallet1.getBalanse());
+//wallet1.withdraw(600);
+//console.log(wallet1.getBalanse());
+//wallet1.withdraw(100);
+
+
