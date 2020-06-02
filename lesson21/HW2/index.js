046c0c8b29d05e35c031d3fdd3de0bd2f782c7e7
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-02 17:11:56 +0300

    commit

---
+++ lesson21/HW2/index.js
@@ -0,0 +1,20 @@
+
+function getItemsList() {
+    const elementsList = document.querySelectorAll('.technology');
+    console.dir(elementsList);
+    return elementsList;
+};
+
+
+function getItemsArray() {
+    const elementsArray = Array.from (document.querySelectorAll('.tool'));
+   
+    
+    console.dir(elementsArray);
+
+
+    return elementsArray;
+}
+
+//getItemsList();
+//getItemsArray();
