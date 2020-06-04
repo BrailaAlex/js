c3053ac3bd1ed88e7db11cdbfcb707435660459d
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-04 04:58:03 +0300

    commit

---
+++ lesson21/HW11/index.js
@@ -0,0 +1,11 @@
+const squaredNumber = () => {
+
+    const el = [...document.querySelectorAll('.number')];
+    const num = el.map(el => el.dataset.number);
+    const numSqut = num.map(el => el * el); // **2
+    return el.map((el, index) => el.dataset.numSqut = numSqut[index])
+
+}
+
+
+//squaredNumber();
