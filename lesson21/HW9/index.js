12e36393707f488758c2f26c03a9580e313a99a2
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-04 03:24:36 +0300

    commit

---
+++ lesson21/HW9/index.js
@@ -0,0 +1,13 @@
+ export function finishForm(){
+    const atribute = document.querySelector('input[type = text]');
+    atribute.type = 'password';
+
+    const endLi = document.createElement('input');
+        endLi.setAttribute('name', 'login');
+
+    const input = document.querySelector('.login-form');
+   input.prepend(endLi);
+
+}
+
+//finishForm();
