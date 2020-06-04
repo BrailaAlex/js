25c36218dcb82f0620d0bec735f67302fc13d74f
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-04 20:52:28 +0300

    commit

---
+++ lesson22/HW4/index.js
@@ -0,0 +1,6 @@
+const text1 = document.querySelector('.search__btn');
+
+const text2 = document.querySelector('.search__input')
+textElem.addEventListener('click', () => {
+    console.log(text2.value)
+})
