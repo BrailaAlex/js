9de3cfad412689138de81151ea65323a522a6f35
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-08 16:34:44 +0300

    commit

---
+++ lesson24/task1/index.js
@@ -0,0 +1,14 @@
+
+const weekDays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
+
+const DayOfWeek = (date, days) => {
+    const day = new Date(date).getDate();
+    const dateInFuture = new Date(date).setDate(day + days);
+
+    return weekDays[new Date(dateInFuture).getDay()];
+}
+const result = DayOfWeek(new Date(2019, 0, 1), 14);
+
+console.log(result);
+
+export {DayOfWeek};
