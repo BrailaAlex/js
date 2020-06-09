d82195b677ca6d36b0e1956730908fa29c3b2807
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-09 14:45:03 +0300

    commit

--- lesson24/task2/index.js
+++ lesson24/task2/index.js
@@ -4,6 +4,6 @@ const formatter = new Intl.DateTimeFormat('en', {
     hour12: false
 });
 
-export const getTime = date => formatter.format(date);
+export const getGreenwichTime = date => formatter.format(date);
 
 console.log((getTime(new Date())));
