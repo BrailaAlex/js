fb173ad1ac668c049f3cf94f0e8e80c001a34733
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-11 21:06:14 +0300

    commit

--- standups/10.06.2020/task1/index.js
+++ standups/10.06.2020/task1/index.js
@@ -13,19 +13,45 @@ let user = {
 //input: func, obj;
 //output: func;
 
-function myBind1(func, context){
-    return func.bind(context);
+//function myBind1(func, context){
+    //return func.bind(context);
 
 }
-//test data
-let bindedPrintMessage = myBind1(printMessage, user);
-bindedPrintMessage(50, 'Kiev')
 
 //input: func, obj;
 //output: func;
+
+//algo:
+// 1. add func into context;
+// 2. return func;
+
+
+
 function myBind2(func, context){
-    //TODO implement it
+    // 1. add func into context;
+    // 2. call func and return result;
+    return function(...args){
+        let copyContext = Object.assign({}, context);
+
+        copyContext.tempFunc = func;
+
+        console.log(copyContext);
+        //console.log(args);
+
+        return copyContext.tempFunc(...args);
+          
+    }
+    
+}
+//input: func, obj;
+//output: func;
+function myBindWithApply(func, context){
+    return function (...args){
+        let funcResult = func.apply(context, args);
+        return funcResult;
+    }
 }
 
 //test data
-let bindedPrintMessage = my
+let bindedPrintMessage = myBind1(printMessage, user);
+bindedPrintMessage(50, 'Kyiv');
