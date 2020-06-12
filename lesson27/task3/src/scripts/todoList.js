b68376579ba05cb24b66786396ea3eba7b52e97d
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-12 04:56:16 +0300

    commit

--- lesson27/task3/src/scripts/todoList.js
+++ lesson27/task3/src/scripts/todoList.js
@@ -4,6 +4,8 @@ import { onToggleTask} from './updateTask.js';
 export const initTodoListHandlers = () => {
     const createBtnElem = document.querySelector('.create-task-btn');
     createBtnElem.addEventListener('click', onCreateTask);
+
+    
     const initTodoListElem = document.querySelector('.list');
     initTodoListElem.addEventListener('click', onToggleTask);
 }
--- lesson27/task3/src/scripts/updateTask.js
+++ lesson27/task3/src/scripts/updateTask.js
@@ -2,5 +2,28 @@ import { renderTasks } from './renderer.js';
 import { getItem, setItem } from './storage.js';
 
 export const onToggleTask = e => {
-    const isCheckbox = e.target.classList.contains()
+    const isCheckbox = e.target.classList.contains('list')
+
+    if (!isCheckbox) {
+        return;
+    }
+
+    const taskList = getItem('taskList');
+    const newTaskList = taskList
+        .map(task => {
+            if (task.id === e.target.dataset.id) {
+                return {
+                    ...task,
+                    done,
+                    finishDate: done
+                        ? new Date().toISOString()
+                        : null
+                };
+            }
+            return task;
+
+        });
+
+    setItem('tasksList', newTaskList);
+    renderTasks();
 }
