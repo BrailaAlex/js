35fe657a054c8d2e8105018e4d37003897727d4b
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-12 12:35:22 +0300

    commit

--- standups/12.06.2020/index.js
+++ standups/12.06.2020/index.js
@@ -1,19 +1,35 @@
-function positiveSum(arr) {
 
-    return arr.reduce((acc, current) => {
-        if (current < 0) {
-            return
-        } else {
-           return acc +current;
-        }
+//You get an array of numbers, return the sum of all of the positives ones.
+//function positiveSum(arr) {
+//return arr.reduce((acc, el) => el >= 0 ? acc + el : acc, 0)
+//}
 
-    }, 0);
+/*function getSum(a, b) {
+    let sum = 0;
+    if(a ==b ){
+        return a, b;
+    }else{
+        return 
+    }
     
-}
 
-//1. learn requirements
-//2. create step by step algo (& input/output for funtions) 
-//3. create prototype solution
-//4. testing 
-//5. refactoring
-//6. final solution 
+}*/
+
+//Create a function named divisors/Divisors that takes an integer n > 1 and returns an array
+// with all of the integer's divisors(except for 1 and the number itself), from smallest to largest.
+// If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>,
+// String> in Rust).
+
+
+/*function divisors(integer) {
+    let arr = [];
+    for (let i = 2; i < integer; i++) {
+        if (integer % 1 === 0) {
+            arr.push(i);
+        }
+    }
+    return arr.length ===0 ? `${integer} is prime` : arr;
+
+      
+   
+};*/
