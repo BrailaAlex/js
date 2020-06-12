284eb14a65118b2c35b94592dedf930c523b6d0f
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-12 11:30:39 +0300

    commit

--- lesson27/task3/src/scripts/createTask.js
+++ lesson27/task3/src/scripts/createTask.js
@@ -1,28 +1,18 @@
-import { renderTasks } from './renderer.js';
-import { getItem, setItem } from './storage.js';
-
+import { renderTasks } from './renderTask';
+import { getItem, setItem } from './storage';
 export const onCreateTask = () => {
     const taskTitleInputElem = document.querySelector('.task-input');
-
     const text = taskTitleInputElem.value;
-
     if (!text) {
         return;
-    }
-
+    };
     taskTitleInputElem.value = '';
-    const tasksList = getItem('tasksList') || [];
-
-    const taskList = taskList.concat({
-        text,
-        done: false,
-        createDate: new Date().toISOString(),
-        id: Math.random().toString()
-
+    const taskList = getItem('tasksList') || [];
+    const newTasksList = taskList.concat({
+        text, done: false, date: new Date().toISOString(),
+        id: Math.random().toString(),
+        doneDate: null,
     });
-
     setItem('tasksList', newTasksList);
-
     renderTasks();
-
 };
--- lesson27/task3/src/scripts/todoList.js
+++ lesson27/task3/src/scripts/todoList.js
@@ -1,11 +1,8 @@
-import { onCreateTask } from './createTask.js';
-import { onToggleTask} from './updateTask.js';
-
-export const initTodoListHandlers = () => {
-    const createBtnElem = document.querySelector('.create-task-btn');
-    createBtnElem.addEventListener('click', onCreateTask);
-
-    
-    const initTodoListElem = document.querySelector('.list');
-    initTodoListElem.addEventListener('click', onToggleTask);
-}
+import { initTodoListHandlers } from './todoList';
+import { renderTasks } from './renderTask';
+document.addEventListener("DOMContentLoaded", () => {
+    renderTasks();
+    initTodoListHandlers();
+});
+const onStorageChange = (e) => { if (e.key === 'tasksList') renderTasks(); };
+window.addEventListener('storage', onStorageChange);
