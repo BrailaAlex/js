1805be0de877de25e85ea38f26e93c7d21add08e
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-14 19:06:18 +0300

    commit

--- lesson28/task4/index.js
+++ lesson28/task4/index.js
@@ -1,32 +1,17 @@
-//calc(3).add(2).mult(4).div(10).substract(5).result(); //-3
+const add3 = value => value + 3;
+const mult2 = value => value * 2;
+const div4 = value => value / 4;
 
- export const calc = initValue => {
-    let result = initValue;
-    const calculator = {
-        add(value){
-            result += value;
-            return this;
-        },
-        mult(value){
-            result *= value;
-            return this;
-        },
-        div(value){
-            result /= value;
-            return this;
-        },
-        subtract(value){
-            result -= value;
-            return this;
-        },
-        result(){
-           return result;
-        },
+export const compose = (...funcs) => value => {
+    return funcs.reduce((acc, func) => func(acc), value)
 
-    };
-    return calculator;
 };
 
-const result = calc(3).add(2).mult(4).div(10).subtract(5).result();
+const doEverything = compose(
+    add3,
+    mult2,
+    div4,
+);
 
-console.log(result);
+
+//console.log(doEverything(3));
