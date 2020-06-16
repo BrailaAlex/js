11630aa340c5eed4dd0e0d48417ba7e3d8c1d1a5
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-16 03:38:40 +0300

    commit

--- lesson29/task2/index.js
+++ lesson29/task2/index.js
@@ -1,39 +1,14 @@
- export function shmoment(value) {
-    let currentDate = new Date(value);
 
-    let counter = {
-        add(numb, value) {
-            let addTime = {
-                milliseconds: (value) => (currentDate.setMilliseconds(currentDate.getMilliseconds() + value)),
-                seconds: (value) => (currentDate.setSeconds(currentDate.getSeconds() + value)),
-                minutes: (value) => (currentDate.setMinutes(currentDate.getMinutes() + value)),
-                hours: (value) => (currentDate.setHours(currentDate.getHours() + value)),
-                days: (value) => (currentDate.setDate(currentDate.getDate() + value)),
-                months: (value) => (currentDate.setMonth(currentDate.getMonth() + value)),
-                years: (value) => (currentDate.setFullYear(currentDate.getFullYear() + value)),
-            }
-            currentDate = new Date(addTime[numb](value));
-            return counter;
-        },
-        
-        subtract(numb, value) {
-            let time = {
-                milliseconds: (value) => (currentDate.setMilliseconds(currentDate.getMilliseconds() - value)),
-                seconds: (value) => (currentDate.setSeconds(currentDate.getSeconds() - value)),
-                minutes: (value) => (currentDate.setMinutes(currentDate.getMinutes() - value)),
-                hours: (value) => (currentDate.setHours(currentDate.getHours() - value)),
-                days: (value) => (currentDate.setDate(currentDate.getDate() - value)),
-                months: (value) => (currentDate.setMonth(currentDate.getMonth() - value)),
-                years: (value) => (currentDate.setFullYear(currentDate.getFullYear() - value)),
-            }
-            currentDate = new Date(time[numb](value));
-            return counter;
-        },
-        result() {
-            return currentDate;
-        },
-    }
-    return counter;
+const pinger = (num, period) => {
+    const interval = setInterval(() => {
+        console.log('Ping');
+
+    }, period);
+    setTimeout(()=>{
+        clearInterval(interval);
+
+    }, num * period);
+   
+    
 }
-
-//console.log(shmoment(new Date(2020, 0, 7, 17, 17, 17)).add('minutes', 2).add('days', 8).subtract('years', 1).result());
+//pinger(5, 1000);
