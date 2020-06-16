7d965f31efbeadb57139d733f7b3cd5bb3ab67f2
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-16 10:26:41 +0300

    commit

---
+++ standups/16.06.2020/task1/index.js
@@ -0,0 +1,42 @@
+
+function User(name, age) {
+    //let user = {};
+    //Object.assign(user, { name });
+    //Object.assign(user, { age });
+    //return user;
+    this.name = name;
+    this.age = age;
+};
+
+User.prototype.sayHi = function sayHi() {
+    console.log(`Hi, I am ${this.name}`);
+
+};
+
+User.prototype.requestNewPhoto = function () {
+    //...;
+};
+
+User.prototype.setAge = function (value) {
+    if(value < 0){
+        return false;
+    }
+
+    this.age = value;
+
+    if (value > 25) {
+        console.log(this);
+        this.requestNewPhoto();
+    }
+    return value;
+};
+
+//console.log(User.prototype);
+let user1 = new User("Test", 50);
+//console.log(user1);
+//user1.sayHi();
+//user1.setAge(30);
+
+//let user2 = new User("Test222", 50);
+//console.log(user2);
+//user2.sayHi();
