774b55a664c5ab8d9b43837a2b9f2ca07a182a02
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-16 15:30:59 +0300

    commit

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -1,22 +1,22 @@
 //add image
 
-export const addImage = imgSrc =>{
-    const p = new Promise((resolve, reject) =>{
+export const addImage = imgSrc => {
+    const p = new Promise((resolve, reject) => {
         const imgElem = document.createElement('img');
-    imgElem.setAttribute('alt', 'My Photo');
-    imgElem.src = imgSrc;
-    
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
+        imgElem.setAttribute('alt', 'My Photo');
+        imgElem.src = imgSrc;
 
-    const onImageLoaded = () => {
-        const { width, height } = imgElem;
-        resolve(null, { width, height });
-    };
-    imgElem.addEventListener('load', onImageLoaded);
+        const containerElem = document.querySelector('.page');
+        containerElem.append(imgElem);
 
-    imgElem.addEventListener('error', () => reject(new Error('Image load failed')));
-        
+        const onImageLoaded = () => {
+            const { width, height } = imgElem;
+            resolve({ width, height });
+        };
+        imgElem.addEventListener('load', onImageLoaded);
+
+        imgElem.addEventListener('error', () => reject(new Error('Image load failed')));
+
 
     });
     return p;
@@ -26,4 +26,9 @@ export const addImage = imgSrc =>{
 const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
 
 const result = addImage(imgSrc);
-console.log(result);
+
+result.then(data => console.log(data));
+result.catch(error => console.log(error));
+
+
+//console.log(result);
