a264efcbf7cdec5e17af2f8be5dce8e0b5b60a15
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-19 17:23:16 +0300

    commit

--- lesson32/task2/index.js
+++ lesson32/task2/index.js
@@ -18,17 +18,14 @@ const request = url => new Promise(resolve => {
 });
 
 const servers = [
-    'https://server.com/us',
-    'https://server.com/eu',
-    'https://server.com/au',
+    'https://server.com/us/userId',
+    'https://server.com/eu/userId',
+    'https://server.com/au/userId',
 ];
-
-
-
 
 export const getUserASAP = userId => {
     const userUrls = servers
-    .map(serverUrl => `${serverUrl}/users/${userId}`);
+    .map(serverUrl => `${serverUrl}/1/${userId}`);
 
     const requests = userUrls
     .map(userUrl => request(userUrl));
