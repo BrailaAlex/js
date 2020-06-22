14b2a229587aa95f97961d3b69f5d4bd1f05a850
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-22 17:23:16 +0300

    commit

--- lesson33/HW1/index.js
+++ lesson33/HW1/index.js
@@ -2,11 +2,13 @@ const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 
 export const getTasksList = () => fetch(baseUrl)
 .then(response => response.json());
-// .then(result => console.log(result))
+
+//getTasksList(baseUrl).then(taskList => console.log(taskList));
+
 
 
 
 export const getTaskById = id => fetch(`${baseUrl}/${id}`)
     .then(response => response.json());
-    // .then(result => console.log(result))
+    //getTaskById(5).then(taskData => console.log(taskData));
 
