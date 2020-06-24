40007ce1130f4fc41d307c386cab5b48182dc976
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-24 10:12:43 +0300

    commit

---
+++ standups/24.06.2020/task1/index.js
@@ -0,0 +1,6 @@
+const handleClick =  () => {
+    if (event.target.classList.contains('list-item')) {
+        console.log(event.target.textContent);
+    } 
+};
+document.querySelector('.list').addEventListener('click', handleClick);
