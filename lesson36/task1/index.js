e6d4a6ae6b013898a32a5d131692969e0375164c
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-06-27 11:43:00 +0300

    commit

---
+++ lesson36/task1/index.js
@@ -0,0 +1,18 @@
+export const fetchUser = async USERID =>{
+    try{
+        const response = await fetch(`https://api.github.com/users/${USERID}`)
+    if(!response.ok){
+        return null;
+    }
+       
+    const userData = await response.json();
+    return userData;
+}catch(err){
+    throw new Error('Failed to fetch user')
+    
+}
+  
+};
+
+fetchUser('facebook');
+//console.log(fetchUser('facebook'));
