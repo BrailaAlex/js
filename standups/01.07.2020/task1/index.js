7e5e97f0744d7e98ec0c7df14f697e002139e123
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-07-01 19:24:32 +0300

    commit

--- standups/01.07.2020/task1/index.js
+++ standups/01.07.2020/task1/index.js
@@ -1,6 +1,6 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockap.io/api/v1';
 
-export function getUserData(userId) {
+/*export function getUserData(userId) {
     return fetch(`${baseUrl}/users/${userId}`).then(response => {
         if (!response.ok) {
             throw new Error('Can not find a user');
@@ -19,5 +19,35 @@ export function printUserData(userId) {
         });
 }
 
+printUserData(25)*/
+
+export async function getUserData(userId) {
+    try {
+        const request = await fetch(`${baseUrl}/users/${userId}`);
+
+        const response = await request;
+        
+        return response.json()
+    } catch (error) {
+        throw new Error('Can not find a user');
+
+    }
+}
+
+export async function printUserData(userId) {
+    try {
+        const response = getUserData(userId);
+
+        let request = await response;
+
+        console.log(request)
+    } catch (error) {
+        console.log(error.message)
+    }
+
+}
 //printUserData(25)
+
+
+
 
